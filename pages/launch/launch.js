ccef8c0035d558b55174f208cfc2e13f698917f6
leyan95 <18231926271@163.com>
2019-10-24 00:44:38 +0800

    🧙‍♂️ Let me predict your fate.
    啊哈~

--- pages/launch/launch.js
+++ pages/launch/launch.js
@@ -2,72 +2,63 @@ const app = getApp();
 Page({
 
   data: {
-    showDialog: false,
-    banners: ['', '', ''],
+    banners: ['../images/activity_selected.png', '../images/activity_selected.png', '../images/activity_selected.png'],
     swiperMaxNumber: 3,
-    swiperCurrent: 0
+    swiperCurrent: 0,
+    authorized: false
   },
 
   onLoad: function(options) {
+    var that = this;
+    if (!that.data.authorized) {
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            that.setData({
+              authorized: true
+            });
+          }
+        }
+      });
+    }
+  },
 
+  goToIndex: function() {
+    var that = this;
+    wx.getUserInfo({
+      success: res => {
+        app.globalData.userInfo = res.userInfo
+        that.loginBackend();
+        if (this.userInfoReadyCallback) {
+          this.userInfoReadyCallback(res)
+        }
+      }
+    });
   },
 
   onGotUserInfo: function(e) {
     var that = this;
-    that.setData({
-      showDialog: false
-    });
     if (e.detail.userInfo) {
-      app.globalData.userInfo = e.detail.userInfo
-      wx.reLaunch({
-        url: '/pages/index/index',
-      });
+      app.globalData.userInfo = e.detail.userInfo;
+      that.loginBackend();
     }
+  },
+
+  loginBackend: function() {
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.reLaunch({
+          url: '/pages/index/index',
+        });
+      }
+    });
   },
 
   swiperchange: function(e) {
     this.setData({
       swiperCurrent: e.detail.current
     })
-  },
-
-  goToIndex: function() {
-    var that = this;
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          wx.getUserInfo({
-            success: res => {
-              app.globalData.userInfo = res.userInfo
-              console.log(res);
-
-              // 登录
-              wx.login({
-                success: res => {
-                  // 发送 res.code 到后台换取 openId, sessionKey, unionId
-                  wx.reLaunch({
-                    url: '/pages/index/index',
-                  });
-                }
-              })
-
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        } else {
-          that.setData({
-            showDialog: true
-          });
-        }
-      }
-    })
-  },
-
-  toggleDialog() {
-    this.setData({
-      showDialog: !this.data.showDialog
-    });
   }
 })
