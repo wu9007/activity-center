de1a4ae50378c987165ca1a28402b09d3d481d0f
leyan95 <18231926271@163.com>
2019-10-26 00:35:05 +0800

    🤡scanning and get location
    啊哈~

--- pages/person/info.js
+++ pages/person/info.js
@@ -1,13 +1,47 @@
 const app = getApp();
 Page({
-  data: {
-    userInfo: null
-  },
+    data: {
+        userInfo: null
+    },
 
-  onLoad: function(options) {
-    var that = this;
-    that.setData({
-      userInfo: app.globalData.userInfo
-    });
-  }
+    onLoad: function(options) {
+        var that = this;
+        that.setData({
+            userInfo: app.globalData.userInfo
+        });
+    },
+
+    scanToRegister: function(e) {
+        var that = this;
+        wx.scanCode({
+            success(res) {
+                console.log(res)
+                that.getLocalData();
+            }
+        })
+    },
+
+    getLocalData: function(e) {
+        var document = document;
+        var that = this;
+        var w = wx;
+        wx.getLocation({
+            type: 'gcj02',
+            success(res) {
+                console.log(res);
+            },
+            fail() {
+                w.showActionSheet({
+                    itemList: ['去授权'],
+                    success: function(res) {
+                        that.openSetting();
+                    }
+                })
+            }
+        });
+    },
+
+    openSetting: function() {
+        wx.openSetting()
+    }
 })
